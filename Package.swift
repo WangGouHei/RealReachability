a6d9d3a717d9e63db36849a20b2faa66074c683a
WangGouHei <w986151145@gmail.com>
2023-07-20 18:52:34 +0800

    制作package

--- Package.swift
+++ Package.swift
@@ -2,20 +2,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "RealReachability",
-    platforms: [.iOS(.v9)],
-    products: [
-        .library(
-            name: "RealReachability",
-            targets: ["RealReachability"]
-        ),
+  name: "RealReachability",
+  platforms: [.iOS(.v9), .macOS(.v10_10), .watchOS(.v3), .tvOS(.v9)],
+  products: ([
+    [
+      .library(name: "RealReachability", targets: ["RealReachability"]),
     ],
-    targets: [
+  ] as [[Product]]).flatMap { $0 },
+  targets: ([
+    [
         .target(
             name: "RealReachability",
             dependencies: [],
-            path: "RealReachability/",
-            exclude: ["Info.plist","README.md"]
-        )
-    ]
+            path: "RealReachability",
+            exclude: ["Info.plist","README.md"],
+            publicHeadersPath: "include"
+        ),
+    ],
+  ] as [[Target]]).flatMap { $0 }
 )
